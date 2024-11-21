bb471683c771a0451301a2817764ae55d85c15f6
Gabriel Santos <ebert.gabrielst@gmail.com>
2024-11-21 00:07:27 -0300

    :chore: Finish challenge

--- src/server.js
+++ src/server.js
@@ -27,6 +27,4 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(3333, () => {
-	console.log('Server is running on port http://localhost:3333')
-})
+server.listen(3333, () =>	console.log('Server is running on port http://localhost:3333'))
